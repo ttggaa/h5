04f4e1c8ae9b565b39ee67b106f346ee512b07d2
liuqi2016 <1551255297@qq.com>
2018-11-28 14:01:22 +0800

    微信通道修改

--- h5/dev/application/controllers/Api.php
+++ h5/dev/application/controllers/Api.php
@@ -905,7 +905,7 @@ class ApiController extends Yaf_Controller_Abstract
         $request = $_GET;
         $this->checkParams($request, ['game_id', 'account','password','username','sign','timestamp']);
         //时效验证
-        if(time()-$request['timestamp']>300){
+        if(time()-$request['timestamp']>3000){
             $assign['status'] = 'fail';
             $assign['msg'] = 'time error';
             exit(json_encode($assign));
@@ -929,7 +929,7 @@ class ApiController extends Yaf_Controller_Abstract
             ksort($arr2);
             $sign_key=$arr['key'];
             $md5 = md5(implode('', $arr2) . $sign_key);
-            $sign=$request['timestamp'];
+            $sign=$request['sign'];
             if (strcmp($sign, $md5) != 0) {
                 exit('Sign error.');
             }
