4785442295a2e1e64cf1a3180ffde4c060c409f9
liuqi2016 <1551255297@qq.com>
2019-02-21 17:41:08 +0800

    游戏下载微信限制

--- tg/dev/application/controllers/Index.php
+++ tg/dev/application/controllers/Index.php
@@ -106,7 +106,7 @@ class IndexController extends Yaf_Controller_Abstract
     {
         if( strpos($_SERVER['HTTP_USER_AGENT'], 'MicroMessenger') !== false ) {
             //提示
-            $this->display('wx');
+            $this->getView()->display('index/wx.phtml');
         }else {
             $game_id = $_GET['game_id'] ?? die('游戏id必须');
             $channe_id = $_GET['tg_channel'] ?? 1;
